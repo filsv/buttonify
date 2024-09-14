8e587dd9f5ee7c53f493693fca07f9c78b3ffe20
Fil Sviatoslav <fil@stops.com>
2024-09-14 12:08:06 +0300

    HoverButton updated interactions.

--- Sources/Buttonify/HoverButton/HoverButton.swift
+++ Sources/Buttonify/HoverButton/HoverButton.swift
@@ -7,115 +7,113 @@
 
 import SwiftUI
 
+
 public struct HoverButton<Content: View>: View {
+    // MARK: - Properties
 
     private let style: Style
     private let content: Content
-    private let tapHapticType: HapticType? // Haptic for tap
-    private let releaseHapticType: HapticType? // Haptic for release
-    private let longTapHapticType: HapticType? // Haptic for long tap
-    private let interactionCallback: (InteractionType) -> Void // Callback for interaction
-    private var resetDelay: TimeInterval = 0.5 // Delay to reset interaction to none
+    private let tapHapticType: HapticType?
+    private let longPressHapticType: HapticType?
+    private let releaseHapticType: HapticType?
+    private let interactionCallback: (InteractionType) -> Void
+    private let resetDelay: TimeInterval
 
-    @State private var isActive: Bool = false
+    @State private var isPressed = false
     @State private var interactionType: InteractionType = .none
-    @State private var longPressInProgress: Bool = false // Track long tap
-    @State private var longPressTriggered: Bool = false  // Track if long press was triggered
 
-    public init(style: Style,
-                tapHapticType: HapticType? = nil,
-                releaseHapticType: HapticType? = nil,
-                longTapHapticType: HapticType? = nil,
-                resetDelay: TimeInterval = 0.5,
-                @ViewBuilder content: () -> Content,
-                interactionCallback: @escaping (InteractionType) -> Void) {
+    // MARK: - Initializer
+
+    public init(
+        style: Style,
+        tapHapticType: HapticType? = nil,
+        longPressHapticType: HapticType? = nil,
+        releaseHapticType: HapticType? = nil,
+        resetDelay: TimeInterval = 0.5,
+        @ViewBuilder content: () -> Content,
+        interactionCallback: @escaping (InteractionType) -> Void
+    ) {
         self.style = style
         self.tapHapticType = tapHapticType
+        self.longPressHapticType = longPressHapticType
         self.releaseHapticType = releaseHapticType
-        self.longTapHapticType = longTapHapticType
         self.resetDelay = resetDelay
         self.content = content()
         self.interactionCallback = interactionCallback
     }
 
+    // MARK: - Body
+
     public var body: some View {
-        content
+        Button(action: {
+            handleTap()
+        }) {
+            content
             .font(style.value.font)
-            .padding(style.value.padding ?? 0)
-            .padding(.horizontal, style.value.horizontalPadding ?? 0)
-            .background(isActive ? style.value.background ?? Color.gray : style.value.hoveredBackground ?? Color.clear)
-            .clipShape(RoundedRectangle(cornerRadius: style.value.radius ?? 0, style: .continuous))
-            .foregroundStyle(isActive ? style.value.selectedTint ?? style.value.tint : style.value.tint)
-            .animation(.easeInOut(duration: 0.2), value: isActive)
-            .gesture(
-                SimultaneousGesture(
-                    LongPressGesture(minimumDuration: 0.5)
-                        .onChanged { _ in
-                            handleLongPressChanged()
-                        }
-                        .onEnded { _ in
-                            handleLongPressEnded()
-                        },
-                    TapGesture()
-                        .onEnded {
-                            handleTapEnded()
-                        }
-                )
-            )
-            .onChange(of: isActive) { _ in
-                longPressInProgress = false // Reset long press tracking after interaction
-            }
-            .onHover { isHovering in
-                self.isActive = isHovering
-            }
-    }
-
-    // Long press changed logic
-    private func handleLongPressChanged() {
-        if !longPressInProgress {
-            longPressInProgress = true
-            longPressTriggered = true
-            interactionType = .holding
-            interactionCallback(.holding)
-            triggerHapticIfNeeded(hapticType: longTapHapticType)
+            .padding(style.value.padding ?? 0.0)
+            .padding(.horizontal, style.value.horizontalPadding ?? 0.0)
+            .background(backgroundColor)
+            .foregroundColor(foregroundColor)
+            .clipShape(RoundedRectangle(cornerRadius: style.value.radius ?? 0.0, style: .continuous))
+            .animation(.easeInOut(duration: 0.2), value: isPressed)
         }
+        .simultaneousGesture(
+            LongPressGesture(minimumDuration: 0.5)
+                .onChanged { _ in
+                    isPressed = true
+                }
+                .onEnded { _ in
+                    handleLongPress()
+                    isPressed = false
+                }
+        )
     }
 
-    // Long press ended logic
-    private func handleLongPressEnded() {
-        if longPressTriggered {
-            longPressInProgress = false
-            interactionType = .released
-            interactionCallback(.released)
-            isActive = false
-            resetInteractionTypeWithDelay()
-        }
+    // MARK: - Computed Properties
+
+    private var backgroundColor: Color {
+        let hoveredBackground = style.value.hoveredBackground ?? style.value.background ?? Color.clear
+        let background = style.value.background ?? Color.clear
+        return isPressed ? hoveredBackground : background
     }
 
-    // Tap ended logic
-    private func handleTapEnded() {
-        if !longPressInProgress && !longPressTriggered { // Ensure tap is only handled if no long press occurred
-            interactionType = .tap
-            interactionCallback(.tap)
-            triggerHapticIfNeeded(hapticType: tapHapticType)
-            isActive = true
-            resetInteractionTypeWithDelay()
-        }
-        longPressTriggered = false // Reset long press flag after interaction
+    private var foregroundColor: Color {
+        let pressedTint = style.value.selectedTint ?? style.value.tint
+        let tint = style.value.selectedTint ?? style.value.tint
+        return isPressed ? (pressedTint) : tint
     }
 
-    // Trigger haptic feedback if needed
-    private func triggerHapticIfNeeded(hapticType: HapticType?) {
-        if let haptic = hapticType {
-            HapticManager.triggerHaptic(haptic)
-        }
+
+    // MARK: - Gesture Handlers
+
+    private func handleTap() {
+        interactionType = .tap
+        interactionCallback(.tap)
+        triggerHapticIfNeeded(tapHapticType)
+        resetInteractionTypeWithDelay()
     }
 
-    // Reset the interaction type to none with a delay
+    private func handleLongPress() {
+        interactionType = .holding
+        interactionCallback(.holding)
+        triggerHapticIfNeeded(longPressHapticType)
+        resetInteractionTypeWithDelay()
+    }
+
+    // MARK: - Haptic Feedback
+
+    private func triggerHapticIfNeeded(_ hapticType: HapticType?) {
+        guard let hapticType = hapticType else { return }
+        HapticManager.triggerHaptic(hapticType)
+    }
+
+    // MARK: - Interaction Reset
+
     private func resetInteractionTypeWithDelay() {
         DispatchQueue.main.asyncAfter(deadline: .now() + resetDelay) {
             interactionType = .none
-            interactionCallback(.none) // Notify the parent of the reset
+            interactionCallback(.none)
+            triggerHapticIfNeeded(releaseHapticType)
         }
     }
 }
@@ -144,8 +142,8 @@ struct HoverButtonContainer: View {
             HoverButton(
                 style: .primary,
                 tapHapticType: .impact(.light),
-                releaseHapticType: .selection,
-                longTapHapticType: .impact(.heavy)
+                longPressHapticType: .impact(.heavy),
+                releaseHapticType: .selection
             ) {
                 Text(interactionType.description)
                     .font(.headline)
@@ -167,13 +165,11 @@ struct HoverButtonContainer: View {
     private var interactionColor: Color {
         switch interactionType {
         case .tap:
-            return Color.blue
-        case .released:
-            return Color.green
+            return Color.orange
         case .holding:
             return Color.orange
         case .none:
-            return Color.gray
+            return Color.blue
         }
     }
 }
