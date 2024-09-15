1d6c9a41e5dee61ee4015d09be0899169f3b08e7
Fil Sviatoslav <fil@stops.com>
2024-09-15 12:54:07 +0300

    Fixes and improvments of modifier and button itself.

---
+++ Sources/Buttonify/Environment/ButtonStylesKey.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by FIL SVIATOSLAV on 15/09/2024.
+//
+
+import Foundation
---
+++ Sources/Buttonify/HoverButton/HoverButtonContainer.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  
+//
+//  Created by FIL SVIATOSLAV on 15/09/2024.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
