e74370d70212233692381961382b55cdc8a4bdcc
Fil Sviatoslav <fil@stops.com>
2024-09-15 13:16:57 +0300

    Haptic improvments.

---
+++ Sources/Buttonify/Haptics/HapticConfiguration.swift
@@ -0,0 +1,14 @@
+//
+//  HapticConfiguration.swift
+//  
+//
+//  Created by FIL SVIATOSLAV on 15/09/2024.
+//
+
+import Foundation
+
+public struct HapticConfiguration {
+    var tap: HapticType?
+    var longPress: HapticType?
+    var release: HapticType?
+}
